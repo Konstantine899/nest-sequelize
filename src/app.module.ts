773e3b2a043bc7663a57aa19cc0ec3468e59417c
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-02-16 15:34:37 +0300

    feat: UserModule UserService UserController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,20 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 import { SequelizeModule } from "@nestjs/sequelize";
 
+
 @Module({
-  controllers: [AppController],
-  providers: [AppService], // переиспользуемые части кода которые могут быть использованы в других модулях
-  imports: [
-    SequelizeModule.forRoot({
-      dialect: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "Konstantine899",
-      password: "4343",
-      database: "nest-course",
-      models: [],
-      autoLoadModels: true,
-    }),
-  ],
+  imports: [SequelizeModule.forRoot({
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "Konstantine899",
+    password: "4343",
+    database: "nest-course",
+    models: [],
+    autoLoadModels: true
+  }),UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
