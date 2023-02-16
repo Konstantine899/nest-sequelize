b20f0fcddee2864e3713d0faf9c9fd5cc2809d82
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-02-16 13:23:25 +0300

    feat: AppModule добавил настройки sequelize для подключения к БД

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
   controllers: [AppController],
   providers: [AppService], // переиспользуемые части кода которые могут быть использованы в других модулях
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "Konstantine899",
+      password: "4343",
+      database: "nest-course",
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
